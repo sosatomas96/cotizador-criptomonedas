f5195f040668d999f03566d74edb708805302352
sosatomas96 <sosatomas96@gmail.com>
2021-05-12 15:43:54 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled from '@emotion/styled';
+import imagen from './cripto.png'
+import Formulario from './components/Formulario';
+
+const Contenedor = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  @media(min-width:992px){
+    display: grid;
+    grid-template-columns: repeat(2,1fr);
+    column-gap: 2rem;
+  }
+`
+
+const Imagen = styled.img`
+  max-width: 100px;
+  margin-top: 5rem;
+`
+
+const Heading = styled.h1`
+  font-family: 'Bebas Neue', cursive;
+  color: #FFF;
+  text-align: left;
+  font-weight: 700px;
+  font-size: 50px;
+  margin-top: 80px;
+
+  &&::after{
+    content: '';
+    width: 100px;
+    height: 6px;
+    background-color: #66A2FE;
+    display: block;
+  }
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Contenedor>
+      <div>
+        <Imagen 
+          src={imagen}
+          alt='imagen criptomonedas'
+        />
+      </div>
+      <div>
+        <Heading>
+          Cotiza criptomonedas al instante
+        </Heading>
+
+        <Formulario />
+      </div>
+    </Contenedor>
   );
 }
 
